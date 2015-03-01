94e372cffdf5375064263dc9989fb4d9321f7056
aylincsknpnr <aylin_comu@hotmail.com>
2015-03-01 03:14:00 +0200

    update related #3

--- tripwire/app.js
+++ tripwire/app.js
@@ -1,4 +1,3 @@
-
 //storage file list 
 function listContents(storagename) {
   console.log("app lunch");
@@ -23,7 +22,6 @@ function listContents(storagename) {
 			}
 		}
 }
-
 //button click event
 $('input.btn').live('click', function() {
   console.log("buton click");
@@ -34,41 +32,28 @@ $('input.btn').live('click', function() {
 		$('#results').html("start")
 		fileNames.push($(this).val());
 	});
-	
+	//is printed on the selected files in the file
 	var fileString = fileNames.join("\n");
 	var file = new Blob([fileString], {type: "text/plain"});
 	//files named with the time information
 	var sdcard = navigator.getDeviceStorage("sdcard");
 	var request = sdcard.addNamed(file, d.yyyymmdd()+".txt");
-	//var request = sdcard.addNamed(file, "2015.02.25-23.41.03.txt");
-	//is printed on the selected files in the file
 	request.onsuccess = function () {  
-		// for(i=0;i<$('input.sec:checked').length;i++){
-		// var temp = new Array();
-		//temp = $(this).split(' ');
 		var name = $(this).result;
-		// var name2=name.toString();
-		//var name=$('input.sec:checked').val()
-		//.toString();
 		$('#results').html("yazıldı");
-		console.log('File "' + name + '" successfully wrote on the sdcard storage area');
-		//alert(name);   
+		console.log('File "' + name + '" successfully wrote on the sdcard storage area');  
 	}
-	
 	// An error typically occur if a file with the same name already exist
 	request.onerror = function () {
 		$('#results').html("hata")
 		console.warn('Unable to write the file: ' + this.error);
 	}
-
 	$('#results').html("text")
 }); 
-
 //listed files stored on sdcard
 $(document).ready(function(){
 	listContents("sdcard");
 });
-
 //Retrieving current date time information
 Date.prototype.yyyymmdd = function() {
 	var yyyy = this.getFullYear().toString();
@@ -77,7 +62,6 @@ Date.prototype.yyyymmdd = function() {
 	var h = this.getHours().toString();
 	var m = this.getMinutes().toString();
 	var s = this.getSeconds().toString();
-
 	return yyyy + "." + (mm[1]?mm:"0"+mm[0]) + "." + (dd[1]?dd:"0"+dd[0]) + "-" + (h[1]?h:"0"+h[0]) +"." + (m[1]?m:"0"+m[0]) +"." + (s[1]?s:"0"+s[0]); // padding
 };
 d = new Date();
