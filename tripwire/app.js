9c5c84d74fc4bf888dee261aec01050af2dbf90f
aylincsknpnr <aylin_comu@hotmail.com>
2015-03-10 18:17:24 +0200

    dosya ve dizinler iframe içine eklendi, düzenlendi related #3

--- tripwire/app.js
+++ tripwire/app.js
@@ -10,11 +10,18 @@ function listContents(storagename) {
 			if (file != null) {
 				//file name add checkbox
 				$("<label><p><input type='checkbox' class='sec' name='file[]' value='" + file.name + "'/>" + file.name + "</p></label>").appendTo('#results');
+				var res=$("#results").html();
+		var ifrm = document.getElementById('content');
+	ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
+	ifrm.document.open();
+	ifrm.document.write(res);
+	ifrm.document.close();			
+				//.appendTo('#content');	
 				this.done = false;
 			}
 			else {
 				//save button 
-				$('<p><input type="button" class="btn btn-primary" value="Kaydet" /></p>').appendTo('#results');
+				$('<p><input type="button" class="btn btn-primary" value="Kaydet" /></p>').appendTo('#result');
 				this.done = true;
 			}
 			if (!this.done) {
@@ -22,6 +29,7 @@ function listContents(storagename) {
 			}
 		}
 }
+
 //button click event
 $('input.btn').live('click', function() {
   console.log("buton click");
@@ -40,19 +48,19 @@ $('input.btn').live('click', function() {
 	var request = sdcard.addNamed(file, d.yyyymmdd()+".txt");
 	request.onsuccess = function () {  
 		var name = $(this).result;
-		$('#results').html("yazıldı");
+		$('#results2').html("yazıldı");
 		console.log('File "' + name + '" successfully wrote on the sdcard storage area');  
 	}
 	// An error typically occur if a file with the same name already exist
 	request.onerror = function () {
-		$('#results').html("hata")
+		$('#results2').html("hata")
 		console.warn('Unable to write the file: ' + this.error);
 	}
 	$('#results').html("text")
 }); 
 //listed files stored on sdcard
 $(document).ready(function(){
-	listContents("sdcard");
+listContents('sdcard');	
 });
 //Retrieving current date time information
 Date.prototype.yyyymmdd = function() {
@@ -62,7 +70,7 @@ Date.prototype.yyyymmdd = function() {
 	var h = this.getHours().toString();
 	var m = this.getMinutes().toString();
 	var s = this.getSeconds().toString();
-	return yyyy + "." + (mm[1]?mm:"0"+mm[0]) + "." + (dd[1]?dd:"0"+dd[0]) + "-" + (h[1]?h:"0"+h[0]) +"." + (m[1]?m:"0"+m[0]) +"." + (s[1]?s:"0"+s[0]); // padding
+	return yyyy + "." + (mm[1]?mm:"0"+mm[0]) + "." + (dd[1]?dd:"0"+dd[0]) + "-" + (h[1]?h:"0"+h[0]) +"." + (m[1]?m:"0"+m[0]) +"." + (s[1]?s:"0"+s[0]); 
 };
 d = new Date();
 alert( d.yyyymmdd() );
